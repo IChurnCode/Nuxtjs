25d99eb3363cfff5bbb41626025cafc577ba2c7c
iswarya <rajagopal.iswarya@gmail.com>
2019-02-19 14:10:47 +0530

    simple changes in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <logo />
+      <img src="https://vueschool.io/img/favicons/apple-touch-icon.png" alt="the logo">
       <h1 class="title">
         nuxt-fundamentals
       </h1>
@@ -24,45 +24,45 @@
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<!--<script>-->
+<!--import Logo from '~/components/Logo.vue'-->
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
+<!--export default {-->
+  <!--components: {-->
+    <!--Logo-->
+  <!--}-->
+<!--}-->
+<!--</script>-->
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<!--<style>-->
+<!--.container {-->
+  <!--margin: 0 auto;-->
+  <!--min-height: 100vh;-->
+  <!--display: flex;-->
+  <!--justify-content: center;-->
+  <!--align-items: center;-->
+  <!--text-align: center;-->
+<!--}-->
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+<!--.title {-->
+  <!--font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,-->
+    <!--'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;-->
+  <!--display: block;-->
+  <!--font-weight: 300;-->
+  <!--font-size: 100px;-->
+  <!--color: #35495e;-->
+  <!--letter-spacing: 1px;-->
+<!--}-->
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+<!--.subtitle {-->
+  <!--font-weight: 300;-->
+  <!--font-size: 42px;-->
+  <!--color: #526488;-->
+  <!--word-spacing: 5px;-->
+  <!--padding-bottom: 15px;-->
+<!--}-->
 
-.links {
-  padding-top: 15px;
-}
-</style>
+<!--.links {-->
+  <!--padding-top: 15px;-->
+<!--}-->
+<!--</style>-->
