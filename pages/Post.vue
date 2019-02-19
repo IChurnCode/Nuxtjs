2a77003a81dda3f7f465487f405068c305cf24d3
iswarya <rajagopal.iswarya@gmail.com>
2019-02-19 14:26:09 +0530

    creating a page by creating a component in pages directory.nuxt will create routes

---
+++ pages/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <article>
+      <h1>Have a good day</h1>
+      <p>you are going to have an awesome day today. Smile</p>
+    </article>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Post"
+    }
+</script>
+
+<style scoped>
+
+</style>
